dc0b2040612a798f8ae363219c7e9843207f19a1
LencyBhalodiya <worklency@gmail.com>
2026-02-08 19:41:31 +0530

    started multiple env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestController } from './test/test.controller';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load : [configuration],
+      envFilePath: getEnvFilePath(),
+    }),
+  ],
   controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
+
+function getEnvFilePath() {
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+  return process.env.NODE_ENV?.trim() == 'development' ? '.env.dev' : '.env.production';
+}
---
+++ src/config/configuration.ts
@@ -0,0 +1,5 @@
+
+export default () => ({
+    NODE_ENV: process.env.NODE_ENV,
+    port: parseInt(process.env.PORT ?? '3001', 10),
+});
